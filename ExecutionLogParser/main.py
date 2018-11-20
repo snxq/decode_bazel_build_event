bcca9043088786510b978276a59e21cb439712a3
changhengyuan <changhengyuan@bilibili.com>
2018-11-20 15:02:33 +0800

    fix bugs

--- ExecutionLogParser/main.py
+++ ExecutionLogParser/main.py
@@ -7,15 +7,18 @@ cache = []
 with open("/tmp/exec.log.txt", "r") as f:
     for row in f.readlines():
         if start:
-            for k in ["path", "hash", "remote_cache_hit"]:
-                row = row.strip()
-                if row.startswith(k+":") and row not in cache:
-                    print(row.strip())
-                    cache.append(row.strip())
+            row = row.strip()
+            if row.startswith("path:"):
+                if row not in cache:
+                    cache.append(row)
+                else:
+                    start = False
+                    continue
+            if row.startswith(("path:", "hash:", "remote_cache_hit:")):
+                print(row)
 
         if row.startswith("actual_outputs"):
             start = True
         elif row.startswith("remote_cache_hit"):
-            cache.pop()
-            print("\n")
+            print()
             start = False
